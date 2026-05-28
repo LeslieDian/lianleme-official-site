6a9a42f1c9f79a2cb0b34d8bbaf3d3ed530cd737
lidian <1350970384@qq.com>
2026-05-28 17:30:22 +0800

    修正页面顺序: AI正在重新定义健身显示在最上方

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <Navbar />
-    <AICapabilities />
     <HeroSection />
+    <AICapabilities />
     <MembershipSection />
     <VisionSection />
     <DownloadSection />
